6246bcd319eca9b237f389682cf8d663dc97ea41
RQLMIUEE <riquelme.ct.santos@gmail.com>
2025-10-03 10:48:46 -0300

    fff

--- src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
@@ -3,8 +3,15 @@ package br.com.etec.biblioteca.cadastro;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ClasseInformacoes info_obj =new ClasseInformacoes();
+		ClasseInformacoes info_obj2 = new ClasseInformacoes("Dom Casmurro", "machado de Assis");
+		
+		
+		info_obj.mostrardados();
+		info_obj2.mostrardados();
+		
+		
 	}
 
 }
