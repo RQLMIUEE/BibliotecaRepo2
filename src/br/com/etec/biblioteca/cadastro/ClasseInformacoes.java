501f9c39bf4331ee5c4f4591943354b1b6851ca6
RQLMIUEE <riquelme.ct.santos@gmail.com>
2025-10-03 10:38:23 -0300

    adcionados os geters e seters

--- src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -5,6 +5,7 @@ String tituloat;
 String subtituloat;
 int ndepaginas;
 String autoresat;
+String generosat;
 
 public ClasseInformacoes(){
 	
@@ -14,5 +15,57 @@ public ClasseInformacoes(String autor, String titulo){
 this.autoresat = autor;
 this.tituloat = titulo;
  
+}
+
+public String getTitulo(){
+	return tituloat;
+}
+
+
+public String getTSubtitulos(){
+	return subtituloat;
+}
+
+
+public String getAutores(){
+	return autoresat;
+}
+
+
+public String getGenero(){
+	return generosat;
+}
+
+
+
+public int getNumerodepaginas(){
+	return ndepaginas;
+}
+
+public void settitulos(String title){
+	this.tituloat = title;
+}
+
+public void setSubtitulo(String subtitulo){
+	this.subtituloat = subtitulo;
+}
+public void setAutores(String name){
+	this.autoresat = name;
+}
+
+public void setPaginas(int paginas){
+	this.ndepaginas = paginas;
+}
+
+public void setGeneros(String genero){
+	this.generosat = genero;
+}
+
+public void mostrardados(){
+	System.out.println("Informações do produto");
+	System.out.println(tituloat);
+	System.out.println(subtituloat);
+	System.out.println(autoresat);
+	System.out.println(ndepaginas);
 }
 }
