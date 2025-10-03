024db22bac8438e9c9a82955e4ed157e21e0f3f5
RQLMIUEE <riquelme.ct.santos@gmail.com>
2025-10-03 10:05:53 -0300

    Adicionado CçassePrincipal

---
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -0,0 +1,18 @@
+package br.com.etec.biblioteca.cadastro;
+
+public class ClasseInformacoes {
+String tituloat;
+String subtituloat;
+int ndepaginas;
+String autoresat;
+
+public ClasseInformacoes(){
+	
+}
+
+public ClasseInformacoes(String autor, String titulo){
+this.autoresat = autor;
+this.tituloat = titulo;
+ 
+}
+}
